58f5972a85656f447e6d1805357cdff38df4d004
matt novick <mattnovick@outlook.com>
2016-01-20 16:20:47 -0500

    made sure nav and footer are on all pages, fixed certain bugs

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function subscribed(){
         document.getElementById("subscribe").innerHTML = "Subscribed!";
 }
+
+$(document).ready(function() {
+                $('#videobackground').tubular({videoId: 'oxDGEp_28gQ'}); // where idOfYourVideo is the YouTube ID.
+        });
+
+
+$(window).load(function(){
+            $(".mCustomScrollbar").mCustomScrollbar({theme: "dark"});
+});      
